2e3c4aadf49d912efb6cc3ec7e461a573160ec41
Mary <marianeaugusta38@gmail.com>
2018-02-06 21:11:09 -0200

    alteracao formulario

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
- var myIndex = 0;
+$(function() {
 
- [18:51, 5/2/2018] Dani Curso Programacao: $('.carousel').carousel()
-    //   carousel();
-      
-    //   function carousel() {
-    //       var i;
-    //       var x = document.getElementsByClassName("mySlides");
-    //       for (i = 0; i < x.length; i++) {
-    //          x[i].style.display = "none";  
-    //       }
-          myIndex++;
-          if (myIndex > x.length) {myIndex = 1}    
-          x[myIndex-1].style.display = "block";  
-          setTimeout(carousel, 4000); // Change image every 4 seconds
-}
+      $('.carousel').carousel();
+
+      function buscarCep(cep) {
+            let url = `//viacep.com.br/ws/${cep}/json/`;
+
+            fetch(url)
+                  .then((response) => response.json())
+                  .then((response) => {
+                        $('#pacienteEndereco').val(response.logradouro);
+                        $('#pacienteBairro').val(response.bairro);
+                        $('#pacienteEstado').val(response.uf);
+                        $('#pacienteCidade').val(response.localidade);
+                  })
+                  .catch((err) => console.log(err));
+      }
+
+      $("#cadastroPaciente #pacienteCEP").on('blur', function(event) {
+            if (event.target.value !== '') buscarCep(event.target.value);
+      });
+
+      $("#cadastroPaciente form").on('submit', function(event) {
+            event.preventDefault();
+
+            console.log(JSON.stringify($(event.target).serializeArray()))
+      });
+
+});
