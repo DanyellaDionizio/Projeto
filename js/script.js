32dfcbc284747cca342868d44003ebc7e5ab15e5
Mary <marianeaugusta38@gmail.com>
2018-02-07 17:34:17 -0200

    formulario

--- js/script.js
+++ js/script.js
@@ -1,29 +1,25 @@
-$(function() {
+$('.carousel').carousel();
 
-      $('.carousel').carousel();
+function buscarCep(cep) {
+      let url = `//viacep.com.br/ws/${cep}/json/`;
 
-      function buscarCep(cep) {
-            let url = `//viacep.com.br/ws/${cep}/json/`;
+      fetch(url)
+            .then((response) => response.json())
+            .then((response) => {
+                  $('#pacienteEndereco').val(response.logradouro);
+                  $('#pacienteBairro').val(response.bairro);
+                  $('#pacienteEstado').val(response.uf);
+                  $('#pacienteCidade').val(response.localidade);
+            })
+            .catch((err) => console.log(err));
+}
 
-            fetch(url)
-                  .then((response) => response.json())
-                  .then((response) => {
-                        $('#pacienteEndereco').val(response.logradouro);
-                        $('#pacienteBairro').val(response.bairro);
-                        $('#pacienteEstado').val(response.uf);
-                        $('#pacienteCidade').val(response.localidade);
-                  })
-                  .catch((err) => console.log(err));
-      }
+$("#cadastroPaciente #pacienteCEP").on('blur', function(event) {
+      if (event.target.value !== '') buscarCep(event.target.value);
+});
 
-      $("#cadastroPaciente #pacienteCEP").on('blur', function(event) {
-            if (event.target.value !== '') buscarCep(event.target.value);
-      });
+$("#cadastroPaciente form").on('submit', function(event) {
+      event.preventDefault();
 
-      $("#cadastroPaciente form").on('submit', function(event) {
-            event.preventDefault();
-
-            console.log(JSON.stringify($(event.target).serializeArray()))
-      });
-
+      console.log(JSON.stringify($(event.target).serializeArray()))
 });
